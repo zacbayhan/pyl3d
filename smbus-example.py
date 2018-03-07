0c899ae6cbe90e9b5164d903487191836f2c7061
zacbayhan <zacbayhan@gmail.com>
2018-03-07 04:05:16 -0500

    initial transfer

--- smbus-example.py
+++ smbus-example.py
@@ -4,7 +4,7 @@ import smbus
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
 
-DEVICE_ADDRESS = chr(0x69)      #7 bit address (will be left shifted to add the read write bit)
+DEVICE_ADDRESS = 0x69      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0x00
 DEVICE_REG_LEDOUT0 = 0x1d
 
@@ -19,25 +19,25 @@ REG_REFERENCE   = 0x25
 REG_OUT_TEMP    = 0x26
 REG_STATUS_REG  = 0x27
 
-REG_OUT_X_L     = chr(0x28)
-REG_OUT_X_H     = chr(0x29)
-REG_OUT_Y_L     = chr(0x2A)
-REG_OUT_Y_H     = chr(0x2B)
-REG_OUT_Z_L     = chr(0x2C)
-REG_OUT_Z_H     = chr(0x2D)
+REG_OUT_X_L     = 0x28
+REG_OUT_X_H     = 0x29
+REG_OUT_Y_L     = 0x2A
+REG_OUT_Y_H     = 0x2B
+REG_OUT_Z_L     = 0x2C
+REG_OUT_Z_H     = 0x2D
 
-REG_FIFO_CTRL_REG = chr(0x2E)
-REG_FIFO_SRC_REG  = chr(0x2F)
+REG_FIFO_CTRL_REG = 0x2E
+REG_FIFO_SRC_REG  = 0x2F
 
-REG_INT1_CFG      = chr(0x30)
-REG_INT1_SRC      = chr(0x31)
-REG_INT1_THS_XH   = chr(0x32)
-REG_INT1_THS_XL   = chr(0x33)
-REG_INT1_THS_YH   = chr(0x34)
-REG_INT1_THS_YL   = chr(0x35)
-REG_INT1_THS_ZH   = chr(0x36)
-REG_INT1_THS_ZL   = chr(0x37)
-REG_INT1_DURATION = chr(0x38)
+REG_INT1_CFG      = 0x30
+REG_INT1_SRC      = 0x31
+REG_INT1_THS_XH   = 0x32
+REG_INT1_THS_XL   = 0x33
+REG_INT1_THS_YH   = 0x34
+REG_INT1_THS_YL   = 0x35
+REG_INT1_THS_ZH   = 0x36
+REG_INT1_THS_ZL   = 0x37
+REG_INT1_DURATION = 0x38
 
 
 def who_am_i():
