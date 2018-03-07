c221b853cbad2f84bd2b8727581cb50902063896
zacbayhan <zacbayhan@gmail.com>
2018-03-07 02:46:00 -0500

    initial transfer

--- smbus-example.py
+++ smbus-example.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import smbus
 
-bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-
 DEVICE_ADDRESS = 0x69      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0x00
 DEVICE_REG_LEDOUT0 = 0x1d
@@ -48,6 +46,7 @@ def get_temp():
 
 
 def main():
+    bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
     print 'I am: ', who_am_i()
     print 'Temp: ', get_temp()
